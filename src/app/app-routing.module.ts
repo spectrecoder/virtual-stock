a6ef34cab27e8f8e546cf9a270b9a9005cc5b894
spectrecoder <pong96111kind@gmail.com>
2022-08-27 16:57:19 +0530

    added login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'stocks',component:HomeComponent},
+  {path:'login',component:LoginComponent},
+  {path:'',redirectTo:'login',pathMatch:'full'},
+  {path:'**',redirectTo:'login'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
