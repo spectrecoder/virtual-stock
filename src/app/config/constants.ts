2daf135f328b7c61cf6e3bf9f73f0840551a94bc
spectrecoder <pong96111kind@gmail.com>
2022-10-05 11:00:19 +0530

    get implemented from API(JSON Server)

---
+++ src/app/config/constants.ts
@@ -0,0 +1,6 @@
+import { Injectable } from "@angular/core";
+
+@Injectable()
+export class Constants{
+    public static readonly APIURL:string = 'http://localhost:3000/';
+}
--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,9 @@ import { Injectable } from '@angular/core';
 })
 export class HttpService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  public get(url:string,options?:any){
+    return this.http.get(url,options);
+  }
 }
