28107dc5479df2acdcce3327ae03f9febd5d326b
spectrecoder <pong96111kind@gmail.com>
2023-10-15 12:23:29 +0530

    Added active class dynamically for navigation bar

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Router,NavigationEnd} from '@angular/router'
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-layout',
@@ -6,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./layout.component.css']
 })
 export class LayoutComponent {
-
+  urlText:string = 'home';
+  public constructor(router:Router){
+    router.events.pipe(filter( event => event instanceof NavigationEnd))
+    .subscribe((res) => {
+      if(res instanceof NavigationEnd){
+        console.log(res);
+        this.urlText = res.url;
+      }
+    })
+  }
 }
